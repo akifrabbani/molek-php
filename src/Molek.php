49f06fc95dd5766ce973d5576a23359ee679922f
Mohd Hafizuddin M Marzuki <hafizuddin_83@yahoo.com>
2019-07-13 13:53:50 +0800

    Rework the code

--- src/Molek.php
+++ src/Molek.php
@@ -1,231 +1,360 @@
 <?php
+namespace AkifRabbani\Molek;
 
-namespace Akifrabbani\Molek;
+use DateTime;
 
 class Molek
 {
-
-    private $_rule = [];
+    protected $ruleset;
 
     /**
      * Create a new Molek Instance
      */
-    public function __construct($rule = [])
+
+    public function __construct($ruleset = [])
     {
-        $this->_rule = $rule;
+        $this->ruleset = $ruleset;
+    }
+
+     /**
+     * Set ruleset used by the instance
+     *
+     * @param array $ruleset Ruleset for calculation
+     */
+
+   public function setRuleset($ruleset)
+    {
+        $this->ruleset = $ruleset;
     }
 
     /**
-     * Converts rule to seconds
+     * Converts a time value (minutes / hours / days / months) to seconds
      *
-     * @param array $rule Rule for time difference calculation
+     * @param string $type Time type
+     * @param int $value Time value
      *
-     * @return int Seconds of differences
-     * @return array Seconds of differenes and amount
+     * @return int Time in seconds
      */
 
-    private static function timeToSec($rule, $value_is_amount = false) {
-        $sec_diff = 0;
-
-        $target = 0;
-        $seconds = 0;
-
-        // check if ruled is sec or minutes/hours, if minutes, convert to sec
-        if (isset($rule['second'])) {
-            $seconds = 0;
-            $target = $rule['second'];
-        } elseif (isset($rule['minute'])) {
-            $seconds = 60;
-            $target = $rule['minute'];
-        } elseif (isset($rule['hour'])) {
-            $seconds = 3600;
-            $target = $rule['hour'];
-        } elseif (isset($rule['day'])) {
-            $seconds = 86400;
-            $target = $rule['day'];
-        }
-
-        if ($value_is_amount) {
-            return ['diff' => $seconds, 'price' => $target];
-        } else {
-            $sec_diff = round($target * $seconds);
-            return $sec_diff;
+    protected function timeToSeconds($type, $value)
+    {
+        if ($type === 'second') {
+            return $value;
+        } elseif ($type === 'minute') {
+            return $value * 60;
+        } elseif ($type === 'hour') {
+            return $value * 60 * 60;
+        } elseif ($type === 'day') {
+            return $value * 24 * 60 * 60;
+        } elseif ($type === 'month') {
+            return $value * 30 * 24 * 60 * 60;
         }
     }
 
     /**
-     * Calculate only seconds within operation time
+     * Checks provided date within the rule's days or dates
      *
-     * @param DateTime $start Start datetime
-     * @param DateTime $end End datetime
-     * @param string $operation_hour_start Operation hour start time
-     * @param string $operation_hour_end Operation hour end time
-     * @param int $operation_hours_a_day Operation hours in integer
+     * @param array $rule Rule
+     * @param DateTime $date Date
      *
-     * @return int Seconds of operation time
+     * @return bool True or false
      */
-    
-    private static function operationDayToSec($start, $end, $operation_hour_start, $operation_hour_end, $operation_hours_a_day) {
-        $start_at = clone $start;
-        $end_at = clone $end;
-     
+    protected function checkDateWithinDaysOrDates($date, $rule)
+    {
+        $first_flag = true;
+        $second_flag = true;
+
+        if (isset($rule['days']) && !in_array(strtolower($date->format('D')), $rule['days'])) {
+            $first_flag = false;
+        }
+
+        if (isset($rule['dates']) && !in_array($date->format('Y-m-d'), $rule['dates'])) {
+            $second_flag = false;
+        }
+
+        return $first_flag || $second_flag;
+    }
+
+    /**
+     * Get a rule from the first ruleset
+     *
+     * @param int $duration Time duration
+     * @param DateTime $date Date for rule selection
+     *
+     * @return array|null Rule or null
+     */
+
+    protected function getFirstRule($duration, $date)
+    {
+        if (!isset($this->ruleset['first'])) return null;
+
+        $rules = $this->ruleset['first'];
+        $selected_rule = null;
+
+        foreach ($rules as $key => $rule) {
+            $rule_duration = $this->timeToSeconds($rule['type'], $rule['duration']);
+
+            if ($this->checkDateWithinDaysOrDates($date, $rule)) {
+                if ($duration >= $rule_duration) {
+                    $selected_rule = [
+                        'duration' => $rule_duration,
+                        'price' => $rule['price']
+                    ];
+                }
+            }
+        }
+
+        return $selected_rule;
+    }
+
+    /**
+     * Get a rule from the normal ruleset
+     *
+     * @param DateTime $date Date for rule selection
+     *
+     * @return array|null Rule or null
+     */
+
+    protected function getNormalRule($date)
+    {
+        if (!isset($this->ruleset['normal'])) return null;
+
+        $rules = $this->ruleset['normal'];
+        $selected_rule = null;
+
+        foreach ($rules as $key => $rule) {
+            $rule_interval = $this->timeToSeconds($rule['type'], $rule['interval']);
+
+            if ($this->checkDateWithinDaysOrDates($date, $rule)) {
+                $selected_rule = [
+                    'interval' => $rule_interval,
+                    'price' => $rule['price']
+                ];
+            }
+        }
+
+        return $selected_rule;
+    }
+
+    /**
+     * Get a rule from the max ruleset
+     *
+     * @param int $duration Time duration
+     * @param DateTime $date Date for rule selection
+     *
+     * @return array|null Rule or null
+     */
+
+    protected function getMaxRule($duration, $date)
+    {
+        if (!isset($this->ruleset['max'])) return null;
+
+        $rules = $this->ruleset['max'];
+        $selected_rule = null;
+
+        foreach ($rules as $key => $rule) {
+            $rule_duration = $this->timeToSeconds($rule['type'], $rule['duration']);
+
+            if ($this->checkDateWithinDaysOrDates($date, $rule)) {
+                if ($duration >= $rule_duration) {
+                    $selected_rule = [
+                        'duration' => $rule_duration,
+                        'price' => $rule['price']
+                    ];
+                }
+            }
+        }
+
+        return $selected_rule;
+    }
+
+    /**
+     * Calculate the day's price based on provided rules
+     *
+     * @param int $duration Time duration
+     * @param array|null $first_rule First rule
+     * @param array|null $normal_rule Normal rule
+     * @param array|null $max_rule Max rule
+     *
+     * @return string Price
+     */
+
+    protected function calculateDayPrice($duration, $first_rule, $normal_rule, $max_rule)
+    {
+        $price = '0.00';
+
+        if ($max_rule !== null) {
+            if ($duration >= $max_rule['duration']) {
+                return bcadd($max_rule['price'], 0, 2);
+            }
+        }
+
+        if ($first_rule !== null) {
+            $price = bcadd($price, $first_rule['price'], 2);
+            $duration -= $first_rule['duration'];
+        }
+
+        if ($normal_rule !== null) {
+            $price = bcadd($price, bcmul(bcdiv($duration, $normal_rule['interval'], 10), $normal_rule['price'], 10), 2);
+        }
+
+        return $price;
+    }
+
+    /**
+     * Calculate the duration and price between two dates (inclusive)
+     *
+     * @param DateTime $start_at Start datetime
+     * @param DateTime $end_at End datetime
+     * @param bool $detailed_result Flag for returning detailed result
+     *
+     * @return array Result of total duration & price (and list of days if $detailed_result is true)
+     */
+
+    public function calculate($start_at, $end_at, $detailed_result = false)
+    {
+        $ruleset = $this->ruleset;
+
+        $total_duration = 0;
+        $total_price = '0.00';
+
+        if (isset($ruleset['base_price'])) {
+            $total_price = bcadd($total_price, $ruleset['base_price'], 2);
+        }
+
+        $operation_hour_start = '00:00';
+        $operation_hour_end = '24:00';
+
+        if (isset($ruleset['operation_hours'])) {
+            $operation_hour_start = isset($ruleset['operation_hours']['start']) ? $ruleset['operation_hours']['start'] : $operation_hour_start;
+            $operation_hour_end = isset($ruleset['operation_hours']['end']) ? $ruleset['operation_hours']['end'] : $operation_hour_end;
+        }
+
+        $operation_hours_a_day = (strtotime($operation_hour_end) - strtotime($operation_hour_start)) / 3600;
+
+        $days = [];
+
         if ($start_at->format('Y-m-d') === $end_at->format('Y-m-d')) {
             $operation_start_at = new DateTime($start_at->format('Y-m-d') . ' ' . $operation_hour_start . ':00');
             $operation_end_at = new DateTime($end_at->format('Y-m-d') . ' ' . $operation_hour_end . ':00');
-     
+
             if ($start_at > $operation_start_at) {
                 $operation_start_at = $start_at;
             }
-     
+
             if ($end_at < $operation_end_at) {
                 $operation_end_at = $end_at;
             }
-     
-            return strtotime($operation_end_at->format('Y-m-d H:i:s')) - strtotime($operation_start_at->format('Y-m-d H:i:s'));
+
+            if ($end_at < $operation_start_at) {
+                $operation_end_at = $operation_start_at;
+            }
+
+            $duration = $operation_end_at->getTimestamp() - $operation_start_at->getTimestamp();
+
+            $first_rule = $this->getFirstRule($duration, $start_at);
+            $normal_rule = $this->getNormalRule($start_at);
+            $max_rule = $this->getMaxRule($duration, $start_at);
+
+            $price = $this->calculateDayPrice($duration, $first_rule, $normal_rule, $max_rule);
+
+            $total_duration = $duration;
+            $total_price = bcadd($total_price, $price, 2);
+
+            $days[] = [
+                'start_at' => $operation_start_at->format('Y-m-d H:i:s'),
+                'end_at' => $operation_end_at->format('Y-m-d H:i:s'),
+                'price' => $total_price
+            ];
         } elseif ($start_at->format('Y-m-d') !== $end_at->format('Y-m-d')) {
             $is_adjacent_date = (clone $start_at)->modify('+1 day')->format('Y-m-d') === $end_at->format('Y-m-d');
-     
+
             $first_day_operation_start_at = new DateTime($start_at->format('Y-m-d') . ' ' . $operation_hour_start . ':00');
             $first_day_operation_end_at = new DateTime($start_at->format('Y-m-d') . ' ' . $operation_hour_end . ':00');
-     
+
             if ($start_at > $first_day_operation_start_at) {
                 $first_day_operation_start_at = $start_at;
             }
-     
+
             if ($start_at > $first_day_operation_end_at) {
                 $first_day_operation_end_at = $start_at;
             }
-     
+
             $last_day_operation_start_at = new DateTime($end_at->format('Y-m-d') . ' ' . $operation_hour_start . ':00');
             $last_day_operation_end_at = new DateTime($end_at->format('Y-m-d') . ' ' . $operation_hour_end . ':00');
-     
+
             if ($end_at < $last_day_operation_start_at) {
                 $last_day_operation_start_at = $end_at;
             }
-     
+
             if ($end_at < $last_day_operation_end_at) {
                 $last_day_operation_end_at = $end_at;
             }
-     
-            $first_day_seconds = strtotime($first_day_operation_end_at->format('Y-m-d H:i:s')) - strtotime($first_day_operation_start_at->format('Y-m-d H:i:s'));
-            $last_day_seconds = strtotime($last_day_operation_end_at->format('Y-m-d H:i:s')) - strtotime($last_day_operation_start_at->format('Y-m-d H:i:s'));
-     
-            if ($is_adjacent_date) {
-                return $first_day_seconds + $last_day_seconds;
-            } else {
-                $first_day_after_at = new DateTime((clone $start_at)->modify('+1 day')->format('Y-m-d') . ' ' . $operation_hour_start . ':00');
-                $last_day_before_at = new DateTime((clone $end_at)->modify('-1 day')->format('Y-m-d') . ' ' . $operation_hour_end . ':00');
-     
-                if ($first_day_after_at->format('Y-m-d') === $last_day_before_at->format('Y-m-d')) {
-                    return  $first_day_seconds + ($operation_hours_a_day * 3600) + $last_day_seconds;
-                } else {
-                    $date_diff = $first_day_after_at->diff($last_day_before_at);
-     
-                    return $first_day_seconds + (($date_diff->days + 1) * $operation_hours_a_day * 3600) + $last_day_seconds;
+
+            $first_day_duration = $first_day_operation_end_at->getTimestamp() - $first_day_operation_start_at->getTimestamp();
+            $last_day_duration = $last_day_operation_end_at->getTimestamp() - $last_day_operation_start_at->getTimestamp();
+
+            $first_day_first_rule = $this->getFirstRule($first_day_duration, $start_at);
+            $first_day_normal_rule = $this->getNormalRule($start_at);
+            $first_day_max_rule = $this->getMaxRule($first_day_duration, $start_at);
+
+            $first_day_price = $this->calculateDayPrice($first_day_duration, $first_day_first_rule, $first_day_normal_rule, $first_day_max_rule);
+
+            $last_day_normal_rule = $this->getNormalRule($end_at);
+            $last_day_max_rule = $this->getMaxRule($last_day_duration, $end_at);
+
+            $last_day_price = $this->calculateDayPrice($last_day_duration, null, $last_day_normal_rule, $first_day_max_rule);
+
+            $total_duration = $first_day_duration + $last_day_duration;;
+            $total_price = bcadd($total_price, bcadd($first_day_price, $last_day_price, 2), 2);
+
+            $days[] = [
+                'start_at' => $first_day_operation_start_at->format('Y-m-d H:i:s'),
+                'end_at' => $first_day_operation_end_at->format('Y-m-d H:i:s'),
+                'price' => $first_day_price
+            ];
+
+            if (!$is_adjacent_date) {
+                $in_between_date = (clone $start_at)->modify('+1 day');
+
+                while (true) {
+                    $in_between_duration = $operation_hours_a_day * 3600;
+
+                    $in_between_normal_rule = $this->getNormalRule($in_between_date);
+                    $in_between_max_rule = $this->getMaxRule($in_between_duration, $in_between_date);
+
+                    $in_between_price = $this->calculateDayPrice($in_between_duration, null, $in_between_normal_rule, $in_between_max_rule);
+
+                    $total_duration += $in_between_duration;
+                    $total_price = bcadd($total_price, $in_between_price, 2);
+
+                    $days[] = [
+                        'start_at' => $in_between_date->format('Y-m-d') . ' ' . $operation_hour_start . ':00',
+                        'end_at' => $in_between_date->format('Y-m-d') . ' ' . $operation_hour_end . ':00',
+                        'price' => $in_between_price
+                    ];
+
+                    $in_between_date->modify('+1 day');
+                    if ($in_between_date->format('Y-m-d') === $end_at->format('Y-m-d')) break;
                 }
             }
-     
-        }
-    }
 
-    /**
-     * Calculate based on two dates
-     *
-     * @param DateTime $start Start datetime
-     * @param DateTime $end End datetime
-     * @param boolean $return_detailed True to return detailed array
-     *
-     * @return double Price
-     */
-
-    function calculate($start_date, $end_date, $return_detailed = false) {
-        $final_price = 0;
-
-        $start_time = $start_date->getTimestamp();
-        $end_time = $end_date->getTimestamp();
-
-        // calculate duration differences
-        if (isset($this->_rule['operation'])) {
-            if (isset($this->_rule['operation']['start']) && isset($this->_rule['operation']['end'])) {
-
-                $operation_hours_a_day = abs(strtotime($this->_rule['operation']['end']) - strtotime($this->_rule['operation']['start'])) / (60 * 60);
-
-                $start_date_x = clone $start_date;
-                $end_date_x = clone $end_date;
-                
-                $interval = $this->operationDayToSec($start_date_x, $end_date_x, $this->_rule['operation']['start'], $this->_rule['operation']['end'], $operation_hours_a_day);
-            } else {
-                throw new Exception('Not enough arguement for operation.');
-            }
-        } else {
-            // use normal time
-            $interval  = abs($end_time - $start_time);
+            $days[] = [
+                'start_at' => $last_day_operation_start_at->format('Y-m-d H:i:s'),
+                'end_at' => $last_day_operation_end_at->format('Y-m-d H:i:s'),
+                'price' => $last_day_price
+            ];
         }
 
-        $start_interval = $interval;
+        $result = [
+            'total_duration' => $total_duration,
+            'total_price' => $total_price
+        ];
 
-        // set start price
-        if (isset($this->_rule['start'])) $final_price += $this->_rule['start'];
-
-        // calculate max
-        if (isset($this->_rule['max'])) {
-            if (count($this->_rule['max']) != count($this->_rule['max'], COUNT_RECURSIVE)) {
-                foreach ($this->_rule['max'] as $max_rule) {
-                    
-                    if (!isset($max_rule['price'])) {
-                        throw new Exception('No price set for max rule.');
-                    }
-
-                    $sec_diff = $this->timeToSec($max_rule);
-
-                    if ($interval >= $sec_diff) {
-                        $interval = 0;
-                        $final_price = $max_rule['price'];
-                    }
-                
-                }
-            } else {
-                throw new Exception('Max argument must be a multidimensional array.');
-            }
+        if ($detailed_result) {
+            $result = array_merge(['days' => $days], $result);
         }
 
-        // set by first price
-        if (isset($this->_rule['first'])) {
-            if (count($this->_rule['first']) != count($this->_rule['first'], COUNT_RECURSIVE)) {
-                foreach ($this->_rule['first'] as $first_rule) {
-                    if ($interval > 0) {
-                        if (!isset($first_rule['price'])) {
-                            throw new Exception('No price set for first rule.');
-                        }
-
-                        $sec_diff = $this->timeToSec($first_rule);
-
-                        $interval -= $sec_diff;
-                        $final_price = $first_rule['price'];
-                    }
-                }
-            } else {
-                throw new Exception('First argument must be a multidimensional array.');
-            }
-        }
-
-        // set normal interval calculate difference
-        if (isset($this->_rule['interval']) && $interval > 0) {
-            $sec_diff = $this->timeToSec($this->_rule['interval'], true);
-
-            if ($sec_diff['diff'] != 0) {
-                while ($interval > 0) {
-                    $interval -= $sec_diff['diff'];
-                    $final_price += $sec_diff['price'];
-
-                }
-            }
-        }
-
-        if ($return_detailed) {
-            return ['duration' => $start_interval, 'price' => $final_price];
-        } else {
-            return $final_price;
-        }
+        return $result;
     }
 }
